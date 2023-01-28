e7699792ea3b56dd54c2c8f0959682c7e37b1345
Azert9000 <122997416+Azert9000@users.noreply.github.com>
2023-01-28 16:52:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Site-club-info
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/zahara-nour-club-info-hrrkwk)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import logo from '$lib/images/logo.png'
+</script>
+
+<section>
+	<h1>Salut les Geek</h1>
+	<img alt="logo club informatique" width="200px" src={logo} />
+	<h2>Bienvenue au club informatique du Lycée Voltaire</h2>
+
+	Avec :
+	<ul>
+		<li>ZeMadCoder</li>
+		<li>AliZarrade</li>
+		<li>Yllojoyd</li>
+		<li>Ninoou</li>
+		<li><img alt="Azert9000" widht="60px" src="https://cdn.pixabay.com/photo/2014/05/30/23/21earth-358676_1280.jpg">Azert9000</li>
+		<li>Majdx7</li>
+	</ul>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+</style>
